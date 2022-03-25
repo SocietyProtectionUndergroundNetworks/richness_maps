c394c639848fb7f395c86c4ba695c22d8c76a7a6
johan van den hoogen <vandenhoogen.johan@gmail.com>
2022-03-25 08:38:47 +0100

    jackknife analysis w just env vars

--- 20220308_processing_jackknife_results.R
+++ 20220308_processing_jackknife_results.R
@@ -2,15 +2,33 @@ library(data.table)
 library(tidyverse)
 
 # Raw results have 10 predictions per point (pixel); one for each model in the ensemble. Process by taking the mean for each predicted point
-df <- fread('/Users/johanvandenhoogen/SPUN/richness_maps/data/20220308_AMF_jackknife_results.csv') %>% 
+df <- fread('/Users/johanvandenhoogen/SPUN/richness_maps/data/20220324_AMF_jackknife_results_envOnly.csv') %>% 
   mutate(ID = floor(V1/10)) %>% #add pseudo-ID per pixel
   group_by(ID) %>% 
   mutate(across(c(predicted, AMF_diversity), ~ mean(.x, na.rm = TRUE))) %>% #change to summarise if only one record per sample is needed
   mutate(abs_residual = abs(AMF_diversity - predicted))
 
+# Quick plot
+df %>% 
+  ggplot(aes(x = AMF_diversity, y = predicted)) +
+  geom_point() +
+  geom_smooth(formula = 'y~x', method = 'lm') +
+  geom_abline() +
+  xlim(c(0,180)) + ylim(c(0,180)) +
+  xlab("observed")
 
-df <- fread('/Users/johanvandenhoogen/SPUN/richness_maps/data/20220308_ECM_jackknife_results.csv') %>% 
+# EMF
+df <- fread('/Users/johanvandenhoogen/SPUN/richness_maps/data/20220324_ECM_jackknife_results_envOnly.csv') %>% 
   mutate(ID = floor(V1/10)) %>% #add pseudo-ID per pixel
   group_by(ID) %>% 
-  mutate(across(c(predicted, AMF_diversity), ~ mean(.x, na.rm = TRUE))) %>% #change to summarise if only one record per sample is needed
-  mutate(abs_residual = abs(AMF_diversity - predicted))
+  mutate(across(c(predicted, ECM_diversity), ~ mean(.x, na.rm = TRUE))) %>% #change to summarise if only one record per sample is needed
+  mutate(abs_residual = abs(ECM_diversity - predicted))
+
+# Quick plot
+df %>% 
+  ggplot(aes(x = ECM_diversity, y = predicted)) +
+  geom_point() +
+  geom_smooth(formula = 'y~x', method = 'lm') +
+  geom_abline() +
+  xlim(c(0,500)) + ylim(c(0,500)) +
+  xlab('observed')
