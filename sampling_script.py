6f42fd55d37b60eac2e9b92609e4a94c631b8cbd
johan van den hoogen <vandenhoogen.johan@gmail.com>
2022-11-18 11:21:49 +0100

    reorganise repo and rerun with GFv4 data

--- sampling_script.py
+++ sampling_script.py
@@ -13,8 +13,8 @@ from ctypes import c_int
 from multiprocessing import Value, Lock, Process
 
 ee.Initialize()
-#
-# # // The future climate to use (leading 20years window)
+
+# // The future climate to use (leading 20years window)
 # futureClimate_rcp26_2060 = ee.Image('projects/crowtherlab/t3/CHELSA/Future_BioClim_Ensembles/rcp26_2050s_mean');
 # futureClimate_rcp26_2080 = ee.Image('projects/crowtherlab/t3/CHELSA/Future_BioClim_Ensembles/rcp26_2070s_mean');
 # futureClimate_rcp45_2060 = ee.Image('projects/crowtherlab/t3/CHELSA/Future_BioClim_Ensembles/rcp45_2050s_mean');
@@ -100,7 +100,7 @@ compositeToUse = ee.Image('projects/crowtherlab/Composite/CrowtherLab_Composite_
 # compositeToUse = ee.Image.cat(currentClimate, compositeImg, landUse2015, urban_2015)
 
 # FeatureCollection to sample
-points = ee.FeatureCollection("users/johanvandenhoogen/000_SPUN/tedersoo/all_taxa_tedersoo_Ectomycorrhizal")
+points = ee.FeatureCollection("users/johanvandenhoogen/000_SPUN_GFv4/20221111_GFv4_richness_rarefied")
 
 def FCDFconv(fc):
         features = fc.getInfo()['features']
@@ -155,62 +155,64 @@ def extract_grid(region, points):
                             sleep(idle)
 
 def extract_and_write_grid(n, grids, points, region):
-    region = grids.get(n)
-    results = extract_grid(region, points)
+	region = grids.get(n)
+	results = extract_grid(region, points)
 
-    if len(results) > 0:
-        print("Processed %d features" % len(results))
+	if len(results) > 0:
+		print("Processed %d features" % len(results))
 
-        df = pd.DataFrame([item.split(",") for item in results], columns = BANDS)
-        df.replace('None', np.nan, inplace = True)
-        df.to_csv("data/sampled_data/sampled_%d.csv" % n, index = False)
-
-
+		df = pd.DataFrame([item.split(",") for item in results], columns = BANDS)
+		df.replace('None', np.nan, inplace = True)
+		return df
+	else:
+		pass
 
 def generateGrid(region, size):
-    """Generate a grid covering the region with size*size rectangles"""
-    bins = ee.Number(size)
-    coords = ee.List(region.coordinates().get(0))
-    xs = coords.map(lambda l : ee.List(l).get(0))
-    ys = coords.map(lambda l : ee.List(l).get(1))
+	"""Generate a grid covering the region with size*size rectangles"""
+	bins = ee.Number(size)
+	coords = ee.List(region.coordinates().get(0))
+	xs = coords.map(lambda l : ee.List(l).get(0))
+	ys = coords.map(lambda l : ee.List(l).get(1))
 
-    xmin = ee.Number(xs.reduce(ee.Reducer.min()))
-    xmax = ee.Number(xs.reduce(ee.Reducer.max()))
-    ymin = ee.Number(ys.reduce(ee.Reducer.min()))
-    ymax = ee.Number(ys.reduce(ee.Reducer.max()))
+	xmin = ee.Number(xs.reduce(ee.Reducer.min()))
+	xmax = ee.Number(xs.reduce(ee.Reducer.max()))
+	ymin = ee.Number(ys.reduce(ee.Reducer.min()))
+	ymax = ee.Number(ys.reduce(ee.Reducer.max()))
 
-    dx = xmax.subtract(xmin).divide(bins)
-    dy = ymax.subtract(ymin).divide(bins)
+	dx = xmax.subtract(xmin).divide(bins)
+	dy = ymax.subtract(ymin).divide(bins)
 
-    def f1(n):
-        def f2(m):
-            x1 = xmin.add(dx.multiply(n))
-            y1 = ymin.add(dy.multiply(m))
-            return ee.Geometry.Rectangle([x1, y1, x1.add(dx), y1.add(dy)], None, False)
-        return ee.List.sequence(0, bins.subtract(1)).map(f2)
-    grid = ee.List.sequence(0, bins.subtract(1)).map(f1).flatten().flatten()
-    return grid
+	def f1(n):
+		def f2(m):
+			x1 = xmin.add(dx.multiply(n))
+			y1 = ymin.add(dy.multiply(m))
+			return ee.Geometry.Rectangle([x1, y1, x1.add(dx), y1.add(dy)], None, False)
+		return ee.List.sequence(0, bins.subtract(1)).map(f2)
+	grid = ee.List.sequence(0, bins.subtract(1)).map(f1).flatten().flatten()
+	return grid
 
 @contextmanager
 def poolcontext(*args, **kwargs):
-        """This just makes the multiprocessing easier with a generator."""
-        pool = multiprocessing.Pool(*args, **kwargs)
-        yield pool
-        pool.terminate()
+		"""This just makes the multiprocessing easier with a generator."""
+		pool = multiprocessing.Pool(*args, **kwargs)
+		yield pool
+		pool.terminate()
 
 if __name__ == '__main__':
-        unboundedGeo = ee.Geometry.Polygon([[[-180, 88], [180, 88], [180, -88], [-180, -88]]], None, False);
+		unboundedGeo = ee.Geometry.Polygon([[[-180, 88], [180, 88], [180, -88], [-180, -88]]], None, False);
 
-        GRID_WIDTH = 15  # How many grid cells to use.
-        grids = generateGrid(unboundedGeo, GRID_WIDTH)
-        size = grids.size().getInfo()
-        print("Grid size: %d " % size)
+		GRID_WIDTH = 15  # How many grid cells to use.
+		grids = generateGrid(unboundedGeo, GRID_WIDTH)
+		size = grids.size().getInfo()
+		print("Grid size: %d " % size)
 
-        # How many concurrent processors to use.  If you're hitting lots of
-        # "Too many aggregation" errors (more than ~10/minute), then make this
-        # number smaller.  You should be able to always use at least 20.
-        NPROC = 40
-        with poolcontext(NPROC) as pool:
-                results = pool.map(
-                        partial(extract_and_write_grid, grids=grids, points=points, region=unboundedGeo),
-                        range(0, size))
+		# How many concurrent processors to use.  If you're hitting lots of
+		# "Too many aggregation" errors (more than ~10/minute), then make this
+		# number smaller.  You should be able to always use at least 20.
+		NPROC = 40
+		with poolcontext(NPROC) as pool:
+				results = pool.map(
+						partial(extract_and_write_grid, grids=grids, points=points, region=unboundedGeo),
+						range(0, size))
+				results = pd.concat(results)
+				results.to_csv("data/20221117_GFv4_richness_rarefied_sampled.csv")
