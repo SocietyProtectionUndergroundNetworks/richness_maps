42a08bffa71ef36c4d27a59d6ea6f2d345a28592
johan van den hoogen <vandenhoogen.johan@gmail.com>
2022-12-21 08:45:21 +0100

    rerun with filtered data

--- sampling_script.py
+++ sampling_script.py
@@ -88,8 +88,85 @@ ee.Initialize()
 
 currentClimate = ee.Image('projects/crowtherlab/t3/CHELSA/CHELSA_BioClim_1994_2013_180ArcSec')
 
+# Define distance in meters to gapfill (default = 10000)
+distanceToFillInMeters = 10000
 compositeToUse = ee.Image('projects/crowtherlab/Composite/CrowtherLab_Composite_30ArcSec')
 
+# Function to gapfill missing pixels
+def gapFillAndExtendBounds(compositeToUse):
+	# N.B., This script is based on code from Matt Hancher and includes annotations
+	# from his original example code.
+	# Use a helper function to fill holes and boundaries with the nearest value
+	def fillHolesWithNearestValue(imageToFill):
+		source = imageToFill.mask();
+		# Measure 1000000x the distance to the nearest valid pixel. We pick
+		# a large cost (here 1000000) so that the least-cost path will not be
+		# influenced later by the border pixels.
+		# originally: 1000000
+		cost0 = ee.Image(1000000).where(source, 0).cumulativeCost(source, distanceToFillInMeters);
+
+		# Measure the distance to the nearest pixel plus the half-pixel
+		# traversal to the center of a valid border pixel, which may be
+		# 1/2 or 1/sqrt(2).
+		cost1 = ee.Image(1000000).where(source, 1).cumulativeCost(source, distanceToFillInMeters);
+
+		# Measure the distance to the nearest pixel plus the half-pixel
+		# traversal to center of a valid pixel, where the valid pixel
+		# has a cost equal to its original value.
+		cost2 = imageToFill.unmask(1000000).cumulativeCost(source, distanceToFillInMeters);
+
+		# Finally we can compute the original value of the nearest
+		# unmasked pixel.
+		fill = cost2.subtract(cost0).divide(cost1.subtract(cost0));
+
+		# Fill in the masked pixels.
+		filled = imageToFill.unmask(0).add(fill);
+
+		return filled.copyProperties(imageToFill);
+
+	# Use a helper function to convert an image collection to a multiband image
+	def icToImage(imageCollection):
+
+		# Create an empty image to fill
+		emptyImage = ee.Image(0).rename('BAND_TO_REMOVE');
+
+		# Iterate through the collection to make the new multiband image
+		def functionToIterate(image, result):
+			return ee.Image(result).addBands(image)
+
+		multibandImageToSub = ee.Image(imageCollection.iterate(functionToIterate, emptyImage))
+		multibandImageSelected = multibandImageToSub.select(multibandImageToSub.bandNames().remove('BAND_TO_REMOVE'))
+
+		return multibandImageSelected;
+
+	# Turn the multiband image of interest into an image collection
+	imageCollection = ee.ImageCollection(
+		compositeToUse.bandNames().map(lambda bandName: compositeToUse.select([bandName]).set('imageName', bandName)))
+	# print('Image Collection from Composite',imageCollection)
+
+	# Separate out the images that shouldn't be filled
+	imagesNotToFill = imageCollection.filter(ee.Filter.inList('imageName', covariateList).Not())
+	# print('Images not to Fill',imagesNotToFill);
+
+	imagesNotFilled = icToImage(imagesNotToFill)
+	# print('Images not Filled',imagesNotFilled);
+
+	imagesToFill = imageCollection.filter(ee.Filter.inList('imageName', covariateList))
+	# print('Images to Fill',imagesToFill);
+
+	imagesFilled = imagesToFill.map(fillHolesWithNearestValue)
+	# print('Gap Filled Images',imagesFilled);
+
+	multiBandImageFilled = icToImage(imagesFilled)
+	# print('Multiband image filled',multiBandImageFilled);
+
+	imageToReturn = ee.Image.cat(multiBandImageFilled, imagesNotFilled)
+	# print('Filled Composite',stackedCompositeFilled);
+
+	return imageToReturn
+
+covariateList = compositeToUse.bandNames().getInfo()
+compositeToUse = gapFillAndExtendBounds(compositeToUse)
 # landUse2015 = ee.ImageCollection([
 #   ssp1_rcp26_2015_mean_11PFTs,
 #   ssp2_rcp45_2015_mean_11PFTs,
@@ -215,4 +292,4 @@ if __name__ == '__main__':
 						partial(extract_and_write_grid, grids=grids, points=points, region=unboundedGeo),
 						range(0, size))
 				results = pd.concat(results)
-				results.to_csv("data/20221122_GFv4_richness_rarefied_sampled.csv")
+				results.to_csv("data/20221210_GFv4_richness_rarefied_sampled.csv")
