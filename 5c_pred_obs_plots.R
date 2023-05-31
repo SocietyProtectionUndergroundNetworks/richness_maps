0159ed4f834a825baa1b8f910ad2167a498d563c
johan van den hoogen <vandenhoogen.johan@gmail.com>
2023-05-31 10:42:07 +0200

    Update 5c_pred_obs_plots.R

--- 5c_pred_obs_plots.R
+++ 5c_pred_obs_plots.R
@@ -8,9 +8,8 @@ setwd('/Users/johanvandenhoogen/SPUN/richness_maps')
 paletteForUse <- c('#d10000', '#ff6622', '#ffda21', '#33dd00', '#1133cc', '#220066', '#330044')
 colors <-  colorRampPalette(paletteForUse)(256)
 
-
-
-df <- fread('output/20230501_arbuscular_mycorrhizal_richness_pred_obs.csv')
+# Arbuscular
+df <- fread('output/20230510_arbuscular_mycorrhizal_richness_pred_obs.csv')
 
 df$dens <- col2rgb(densCols(df[['arbuscular_mycorrhizal_richness']], df[['arbuscular_mycorrhizal_richness_Predicted']]))[1,] + 1L
 
@@ -31,10 +30,8 @@ df %>%
         aspect.ratio = 1) +
   labs(y = "Predicted AMF Richness", x = "Observed AMF Richness")
 
-
-
-
-df <- fread('output/20230501_ectomycorrhizal_richness_pred_obs.csv') %>% 
+# Ecto
+df <- fread('output/20230531_ectomycorrhizal_richness_pred_obs.csv') %>% 
   group_by(sample_id) 
 
 df$dens <- col2rgb(densCols(df[['ectomycorrhizal_richness']], df[['ectomycorrhizal_richness_Predicted']]))[1,] + 1L
@@ -58,49 +55,56 @@ df %>%
 
 
 
+##### With MEMs
 
-# df <- fread('data/20230428_AM_predObs_sampled.csv')
-# df
-# 
-# df$dens <- col2rgb(densCols(df[['arbuscular_mycorrhizal_richness']], df[['arbuscular_mycorrhizal_richness_Ensemble_mean']]))[1,] + 1L
-# 
-# # Map densities to colors
-# df$colors = colors[df$dens]
-# 
-# df %>% 
-#   ggplot(aes(x = arbuscular_mycorrhizal_richness, y = arbuscular_mycorrhizal_richness_Ensemble_mean)) +
-#   geom_point(color = df$colors) +
-#   scale_color_manual(values = rev(brewer.pal(6, "Paired"))) +
-#   geom_abline(linetype = 2) +
-#   geom_smooth(method = 'lm', formula = 'y ~ x', color = 'black', lwd = 0.5, se = F) +
-#   # scale_x_log10() + scale_y_log10() +
-#   theme_classic() +
-#   theme(legend.position = "none",
-#         aspect.ratio = 1) +
-#   labs(y = "Predicted AMF Richness", x = "Observed AMF Richness")
-# 
-# 
-# df <- fread('data/20230428_EM_predObs_sampled.csv')
-# 
-# df$dens <- col2rgb(densCols(df[['ectomycorrhizal_richness']], df[['ectomycorrhizal_richness_Ensemble_mean']]))[1,] + 1L
-# 
-# # Map densities to colors
-# df$colors = colors[df$dens]
-# 
-# df %>% 
-#   ggplot(aes(x = ectomycorrhizal_richness, y = ectomycorrhizal_richness_Ensemble_mean)) +
-#   geom_point(color = df$colors) +
-#   scale_color_manual(values = rev(brewer.pal(6, "Paired"))) +
-#   geom_abline(linetype = 2) +
-#   geom_smooth(method = 'lm', formula = 'y ~ x', color = 'black', lwd = 0.5, se = F) +
-#   xlim(c(0, 600)) +
-#   ylim(c(0, 600)) +
-#    # scale_x_log10() + scale_y_log10() +
-#   theme_classic() +
-#   theme(legend.position = "none",
-#         aspect.ratio = 1) +
-#   labs(y = "Predicted ECM Richness", x = "Observed ECM Richness")
-# 
-# 
-# 
+# Arbuscular
+df <- fread('output/20230502_arbuscular_mycorrhizal_richness_pred_obs_wMEM.csv')
+
+df$dens <- col2rgb(densCols(df[['arbuscular_mycorrhizal_richness']], df[['arbuscular_mycorrhizal_richness_Predicted']]))[1,] + 1L
+
+# Map densities to colors
+df$colors = colors[df$dens]
+
+df %>% 
+  ggplot(aes(x = arbuscular_mycorrhizal_richness, y = arbuscular_mycorrhizal_richness_Predicted)) +
+  geom_point(color = df$colors) +
+  scale_color_manual(values = rev(brewer.pal(6, "Paired"))) +
+  geom_abline(linetype = 2) +
+  geom_smooth(method = 'lm', formula = 'y ~ x', color = 'black', lwd = 0.5, se = F) +
+  # scale_x_log10() + scale_y_log10() +
+  xlim(c(0, 150)) +
+  ylim(c(0, 150)) +
+  theme_classic() +
+  theme(legend.position = "none",
+        aspect.ratio = 1) +
+  labs(y = "Predicted AMF Richness", x = "Observed AMF Richness", title = "with MEMs") 
+
+# Ecto
+df <- fread('output/20230531_ectomycorrhizal_wMEM_richness_pred_obs.csv') %>% 
+  group_by(sample_id) 
+
+df$dens <- col2rgb(densCols(df[['ectomycorrhizal_richness']], df[['ectomycorrhizal_richness_Predicted']]))[1,] + 1L
+
+# Map densities to colors
+df$colors = colors[df$dens]
+
+df %>% 
+  ggplot(aes(x = ectomycorrhizal_richness, y = ectomycorrhizal_richness_Predicted)) +
+  geom_point(color = df$colors) +
+  scale_color_manual(values = rev(brewer.pal(6, "Paired"))) +
+  geom_abline(linetype = 2) +
+  geom_smooth(method = 'lm', formula = 'y ~ x', color = 'black', lwd = 0.5, se = F) +
+  xlim(c(0, 600)) +
+  ylim(c(0, 600)) +
+  # scale_x_log10() + scale_y_log10() +
+  theme_classic() +
+  theme(legend.position = "none",
+        aspect.ratio = 1) +
+  labs(y = "Predicted ECM Richness", x = "Observed ECM Richness", title = "with MEMs") 
+
+
+
+
+
+
 
