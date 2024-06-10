46dfac41f02d4b962110c0cefb193728dac0bd92
hooge104 <vandenhoogen.johan@gmail.com>
2024-06-10 10:43:44 +0200

    ECM we map

--- 4_richness_pipeline_EM_we.py
+++ 4_richness_pipeline_EM_we.py
@@ -477,7 +477,7 @@ try:
 
 except Exception as e:
     # Import raw data
-    rawPointCollection = pd.read_csv('data/20240528_EM_richness_rarefied_sampled_oneHot_rwr.csv', float_precision='round_trip')
+    rawPointCollection = pd.read_csv('data/20240528_EM_richness_rarefied_sampled_oneHot_rwr_we.csv', float_precision='round_trip')
     print('Size of original Collection', rawPointCollection.shape[0])
 
     # Rename classification property column
